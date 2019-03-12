861ae630eeb698b7104cafd2d2fe4fed22dca1f6
GeoffroyBERNARD <GeoffroyBERNARD>
2019-03-12 10:59:34 +0100

    added entities

---
+++ API/mobile/src/main/java/geoquizz/mobile/entity/Photo.java
@@ -0,0 +1,76 @@
+package geoquizz.mobile.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Photo {
+
+    @Id
+    private String id;
+    private String desc;
+    private String url;
+    private int x;
+    private int y;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "serie_id", nullable = false)
+    private Serie serie;
+
+    public Photo() { }
+
+    public Photo(String desc, String url, int x, int y) {
+        this.desc = desc;
+        this.url = url;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+}
