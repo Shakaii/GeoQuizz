a9c079e719bac83d9783b82397bd2742491587ba
GeoffroyBERNARD <GeoffroyBERNARD>
2019-03-12 11:29:34 +0100

    fix

--- API/mobile/src/main/java/geoquizz/mobile/bundary/UserResource.java
+++ API/mobile/src/main/java/geoquizz/mobile/bundary/UserResource.java
@@ -1,8 +1,8 @@
 package geoquizz.mobile.bundary;
 
-import geoquizz.mobile.entity.Serie;
+import geoquizz.mobile.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserResource extends CrudRepository<Serie, String> {
+public interface UserResource extends CrudRepository<User, String> {
     //ecrire ici les methods perso (findBy...)
 }
