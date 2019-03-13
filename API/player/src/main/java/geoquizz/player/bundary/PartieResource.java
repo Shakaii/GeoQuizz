c1ce8886cc8826a9cd59a709b2f2c3589107ed8a
Jordan <jordanjezequel@bbox.fr>
2019-03-13 09:52:02 +0100

    Remove unused import

--- API/player/src/main/java/geoquizz/player/bundary/PartieResource.java
+++ API/player/src/main/java/geoquizz/player/bundary/PartieResource.java
@@ -1,7 +1,6 @@
 package geoquizz.player.bundary;
 
 import geoquizz.player.entity.Partie;
-import geoquizz.player.entity.Photo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PartieResource extends CrudRepository<Partie, String> {
