63db5750237d2eacb1ca0c61807cd88a2362e4da
GeoffroyBERNARD <GeoffroyBERNARD>
2019-03-15 11:18:52 +0100

    fixed indent for Serie component

--- Player/src/components/Serie.vue
+++ Player/src/components/Serie.vue
@@ -1,32 +1,28 @@
 <template>
-        <el-button @click="selectSerie(name, id)" v-bind:class="{isSelected: selected}">
-            <h1>{{name}}</h1>
-            <img src="../assets/logo.png">
-        </el-button>
+    <el-button @click="selectSerie(name, id)" v-bind:class="{isSelected: selected}">
+        <h1>{{name}}</h1>
+        <img src="../assets/logo.png">
+    </el-button>
 </template>
 
 <script>
 
-export default {
-  name: 'serie',
-  data() {
-      return {
-          series: [],
-      }
-  },
-  props: ['name', 'selected','id'],
-
-  methods: {
-
-      selectSerie(ville, id){
-          this.selectedSerie = ville;
-          this.$emit('selectSerie', ville)
-          this.$emit('serieId', id);
-      }
-
-  },
-
-}
+    export default {
+        name: 'serie',
+        data() {
+            return {
+                series: [],
+            }
+        },
+        props: ['name', 'selected','id'],
+        methods: {
+            selectSerie(ville, id){
+                this.selectedSerie = ville;
+                this.$emit('selectSerie', ville)
+                this.$emit('serieId', id);
+            }
+        },
+    }
 </script>
 
 <style scoped>
