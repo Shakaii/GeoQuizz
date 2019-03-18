bac937cb64b0e7a93f522a707a1327385ac89620
alexischarlot <alexis.charlot5@etu.univ-lorraine.fr>
2019-03-18 15:04:15 +0100

    debut post d'image mobile

--- GeoQuizzApp/app/components/App.vue
+++ GeoQuizzApp/app/components/App.vue
@@ -2,117 +2,101 @@
     <Page>
         <ActionBar>
             <GridLayout width="100%" columns="auto, *">
-                <Label text="MENU" @tap="$refs.drawer.nativeView.showDrawer()" col="0"/>
-                <Label class="title" text="GeoQuizz Mobile"  col="1"/>
+                <Label text="MENU" @tap="$refs.drawer.nativeView.showDrawer()" col="0" />
+                <Label class="title" text="GeoQuizz Mobile" col="1" />
             </GridLayout>
         </ActionBar>
-       
+
         <RadSideDrawer ref="drawer">
-         
-        
-            <StackLayout  ~drawerContent backgroundColor="#ffffff">
-                <ScrollView >
+
+
+            <StackLayout ~drawerContent backgroundColor="#ffffff">
+                <ScrollView>
                     <StackLayout>
-                    <Label class="drawer-header" @tap="current = 0; $refs.drawer.nativeView.closeDrawer()" text="Series"/>
-                 
-                    <Label v-for='serie in seriesInfo' class="drawer-item" @tap="current = 1;searchSerie(serie._links.self.href);$refs.drawer.nativeView.closeDrawer()" :text='serie.ville'/>
+                        <Label class="drawer-header" @tap="current = 0; $refs.drawer.nativeView.closeDrawer()" text="Series" />
+
+                        <Label v-for='serie in seriesInfo' class="drawer-item" @tap="current = 1;images=[];setSerie(serie.id);searchSerie(serie._links.self.href);$refs.drawer.nativeView.closeDrawer()"
+                            :text='serie.ville' />
                     </StackLayout>
-                 </ScrollView >
-             
+                </ScrollView>
+
             </StackLayout>
             <GridLayout ~mainContent columns="*" rows="*">
-                <Label v-show="current == 0" class="message" :text="msg" col="0" row="0"/>
-                
+               
+                <Label v-show="current == 0" class="message" :text="msg" col="0" row="0" />
                 <StackLayout v-show="current == 1">
-                    <label class="message" :text="serieInfo" col="0" row="0"/>
+                    <label class="message" :text="serieInfo" col="0" row="0" />
+
+
+
                     <Button text="Prendre une photo" @tap="takePicture" />
                     <ScrollView orientation="horizontal">
-                        <WrapLayout>                             
-                            <Image v-for="img in images" :src="img.src" width="150" height="150" />                            
-                        </WrapLayout> 
-                        </ScrollView >
-                        <ScrollView orientation="horizontal">
+                        <WrapLayout>
+                            <Image v-for="img in images" :src="img.src" width="150" height="150" />
+                        </WrapLayout>
+                     
+                    </ScrollView>
+                    <Button v-show="images.length >=1" text="Envoyer les photos prises"  />
+                    <ScrollView orientation="horizontal">
                         <WrapLayout>
                             <Image v-for="ima in seriePhoto" :src="ima.url" width="150" height="150" />
                         </WrapLayout>
-                        </ScrollView >
+                    </ScrollView>
                 </StackLayout>
-            
-          
+
+
             </GridLayout>
-            
+
         </RadSideDrawer>
-      
-        
+
+
     </Page>
 </template>
 
 <script>
-
-import * as camera from "nativescript-camera";
-    import * as imagepicker from "nativescript-imagepicker";
-
+    import * as camera from "nativescript-camera";
+var geolocation = require("nativescript-geolocation");
     import {
         Image
     } from "tns-core-modules/ui/image";
+    
+    export default {
+        name: 'App',
+        components: {
 
-  export default {
-      name: 'App',
-      components: {
-          
-         
-      },
 
-    data() {
-      return {
-        msg: 'Bienvenue sur GeoQuizz Mobile!',
-        current: "",
-        seriesInfo:"",
-        serieInfo:"",
-        seriePhoto:"",
-        images: [],
-      }
-    },
-    methods:{
-        searchSeries(){
-            this.axios
-                .get("https://a8b10422.ngrok.io/office/series")
-                .then(response => (this.seriesInfo = response.data._embedded.series))         
-                .catch(error => {
-                console.log(error);             
-                this.errored = true; 
-            });
         },
-        searchSerie(link){
-             this.axios
-                .get(link)
-                .then(response => (this.serieInfo = response.data.ville,this.seriePhoto = response.data.photos))         
-                .catch(error => {
-                console.log(error);             
-                this.errored = true; 
-            }); 
+
+        data() {
+            return {
+                msg: 'Bienvenue sur GeoQuizz Mobile!',
+                current: "",
+                seriesInfo: "",
+                serieInfo: "",
+                seriePhoto: "",
+                images: [],
+                geocal:0,
+                serieId:"",
+                photoName:"",
+            }
         },
-        selectPicture() {
-                let context = imagepicker.create({
-                    mode: "multiple"
-                });
-
-                context
-                    .authorize()
-                    .then(function() {
-                        return context.present();
-                    })
-                    .then(selection => {
-                        selection.forEach(selected => {
-                            console.log(JSON.stringify(selected));
-
-                            let img = new Image();
-                            img.src = selected;
-                            this.images.push(img);
-                        });
-                    })
-                    .catch(function(e) {
-                        console.log("error in selectPicture", e);
+        methods: {
+            searchSeries() {
+                this.axios
+                    .get("https://a8b10422.ngrok.io/office/series")
+                    .then(response => (this.seriesInfo = response.data._embedded.series))
+                    .catch(error => {
+                        console.log(error);
+                        this.errored = true;
+                    });
+            },
+            searchSerie(link) {
+                this.axios
+                    .get(link)
+                    .then(response => (this.serieInfo = response.data.ville, this.seriePhoto = response.data.photos))
+                    .catch(error => {
+                        console.log(error);
+                        this.errored = true;
                     });
             },
             takePicture() {
@@ -142,19 +126,50 @@ import * as camera from "nativescript-camera";
                     .catch(e => {
                         console.log("Error requesting permission");
                     });
-            }
-    },
-     created:function() {
-        this.searchSeries();
+            },
+            enableLocationTap(args) {
+                geolocation.isEnabled().then(function (isEnabled) {
+                    if (!isEnabled) {
+                        this.geocal=1;
+                        geolocation.enableLocationRequest().then(function () {}, function (e) {
+                            console.log("Error: " + (e.message || e));
+                        });
+                    }
+                }, function (e) {
+                    console.log("Error: " + (e.message || e));
+                });
+            },
+            upPhoto(){
+                this.images.forEach(image => {
+                     this.axios
+                    .post("https://a8b10422.ngrok.io/office/series/"+this.serieId+"/photos",image ,{ headers: {
+                        'Content-type': 'application/x-www-form-urlencoded', }
+                    }) 
+                    .then(response => (this.photoName = response.data.photos))
+                    ;
+                    
+                });
+            },
+            setSerie(serie){
+                this.serieId=serie;
+            },
+            
+        },
+        mounted() {
+         geolocation.enableLocationRequest();
+        },
+        created: function () {
+            this.searchSeries();
+        }
     }
-  }
+
 </script>
 
 <style scoped>
-    ActionBar{
+    ActionBar {
         background-color: teal;
         color: #ffffff;
-      
+
     }
 
     .title {
@@ -172,7 +187,7 @@ import * as camera from "nativescript-camera";
     .drawer-header {
         padding: 50 16 16 16;
         margin-bottom: 16;
-        background-color:teal;
+        background-color: teal;
         color: #ffffff;
         font-size: 24;
     }
@@ -183,5 +198,4 @@ import * as camera from "nativescript-camera";
         font-size: 16;
     }
 
-   
 </style>
