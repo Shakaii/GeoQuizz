00022484d4f6054588bd2ad24f4c4a761f967d5f
a w <antoine.wittmann@hotmail.fr>
2019-03-20 14:11:48 +0100

    ajout des photos, push pas testé (problème auth donc pas testé)

--- GeoQuizzApp/app/components/CreateSerie.vue
+++ GeoQuizzApp/app/components/CreateSerie.vue
@@ -4,26 +4,36 @@
     <StackLayout v-show="active == 0" >
         <Label text="Nom de la série"/>
         <TextField hint="Nom de la série (ville)" autocorrect="false" v-model="name" class="input input-border"></TextField> 
-        <Button text="Prochaine étape" icon="el-icon-arrow-right" @tap="next"/>
+        <Button text="Prochaine étape" @tap="next"/>
     </StackLayout>
     <StackLayout v-show="active == 1">
         <Label text="Géolocalisation de la série"/>
         <TextField hint="Latitude" autocorrect="false" v-model="lat" keyboardType="number" class="input input-border"></TextField> 
         <TextField hint="Longitude" autocorrect="false" v-model="lng" keyboardType="number" class="input input-border"></TextField> 
-        <Button text="Prochaine étape" icon="el-icon-arrow-right" @tap="next"/>
+        <Button text="Prochaine étape" @tap="next"/>
         <Button text="Etape précédente" icon="el-icon-arrow-left" @tap="back"/>
     </StackLayout>
     <StackLayout v-show="active == 2">
         <Label text="Ajout des photos"/>
+        <Button text="Prendre une photo" @tap="takePicture" />
+        <Button text="Importer une photo" @tap="selectPicture" />
+        <Button text="Prochaine étape" @tap="next"/>
+        <Button text="Etape précédente" @tap="back"/>
+    </StackLayout>
+    <StackLayout v-show="active == 3">
+        <Label text="Confirmation"/>
         <Button text="Terminer" @tap="createSerie"/>
-        <Button text="Etape précédente" icon="el-icon-arrow-left" @tap="back"/>
+        <Button text="Etape précédente" @tap="back"/>
     </StackLayout>
 </FlexboxLayout>
 </template>
 
 <script>
-//import Vue2Leaflet from 'vue2-leaflet';
-//import { LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+import * as camera from "nativescript-camera";
+    import * as imagepicker from "nativescript-imagepicker";
+import {
+        Image
+    } from "tns-core-modules/ui/image";
 
 export default {
     name: 'createserie',
@@ -43,7 +53,8 @@ export default {
             center: null,//L.latLng(48.69333, 6.18324),
             lat: null,
             lng: null,
-            imgUrl: ""
+            imgUrl: "",
+            images: [],
         }
     },
     components: {
@@ -51,7 +62,7 @@ export default {
     },
     methods: {
       next() {
-        if (this.active++ > 2) this.active = 0;
+        if (this.active++ > 3) this.active = 0;
         console.log(this.lat+" / "+this.lng);
       },
       back() {
@@ -66,12 +77,63 @@ export default {
         })
         .then((response) => {
             console.log(response.data);
-            this.$router.push("/addPhotos/"+response.data.id)
+            this.$router.push("/addPhotos/"+this.images);
         })
         .catch(function (error) {
             console.log(error);
         });
       },
+      selectPicture() {
+                let context = imagepicker.create({
+                    mode: "multiple"
+                });
+
+                context
+                    .authorize()
+                    .then(function() {
+                        return context.present();
+                    })
+                    .then(selection => {
+                        selection.forEach(selected => {
+                            console.log(JSON.stringify(selected));
+
+                            let img = new Image();
+                            img.src = selected;
+                            this.images.push(img);
+                        });
+                    })
+                    .catch(function(e) {
+                        console.log("error in selectPicture", e);
+                    });
+            },
+            takePicture() {
+                camera
+                    .requestPermissions()
+                    .then(() => {
+                        camera
+                            .takePicture({
+                                width: 300,
+                                height: 300,
+                                keepAspectRatio: true,
+                                saveToGallery: false
+                            })
+                            .then(imageAsset => {
+                                let img = new Image();
+                                img.src = imageAsset;
+                                this.images.push(img);
+                                console.log(
+                                    "ive got " + this.images.length +
+                                    " images now."
+                                );
+                            })
+                            .catch(e => {
+                                console.log("error:", e);
+                            });
+                    })
+                    .catch(e => {
+                        console.log("Error requesting permission");
+                    });
+            },
     }
 }
 </script>
